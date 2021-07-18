d5579a1af4c7c0bd077b324886af48508c1b89a2
maimunayt <83404397+maimunayt@users.noreply.github.com>
2021-07-18 22:43:35 +0100

    add assignmentdb

--- assignmentdb.sql
+++ assignmentdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 04, 2021 at 08:28 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.30
+-- Generation Time: Jul 17, 2021 at 12:25 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,8 +18,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `assignmentdb`
+-- Database: `assigmentdb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `ID` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `fullName` varchar(150) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `pdate` date NOT NULL,
+  `postID` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `ID` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `fullName` varchar(150) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `pdate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -28,11 +57,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `fullname` varchar(50) DEFAULT NULL,
-  `email` varchar(50) NOT NULL,
-  `phone` int(15) NOT NULL,
-  `address` varchar(50) NOT NULL,
-  `password` varchar(60) DEFAULT NULL
+  `fullname` varchar(50) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `address` text NOT NULL,
+  `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -40,10 +69,32 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
